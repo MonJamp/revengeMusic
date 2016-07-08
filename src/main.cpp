98c5540bfd56453e478ac16214e276e5cc216495
monsterjamp <monsterjamp@yahoo.com>
2016-07-08 13:20:59 -0500

    Get directories after checking for multiple instances. You can now kill the original instance without using any arguments

--- src/main.cpp
+++ src/main.cpp
@@ -18,58 +18,56 @@
 
 int main( int argc, char *argv[]) {
 
-    std::string track;
-    std::string music_dir;
-    const char* home_dir;
-    bool running = true;
+    IPC ipc("/tmp/fifo");
 
-    std::string kill = "kill";
-  
-    //Get home directory of user
-    home_dir = getenv("HOME");
-    if(home_dir == NULL) {
-        //Get home directory if it is not defined in the environment variable
-        home_dir = getpwuid(getuid())->pw_dir;
-        if(home_dir == NULL) {
-            perror("Error: could not get home directory");
-            return -1;
+    if(!ipc.isOnlyInstance()) {
+        if(argv[1] == NULL) {
+            ipc.SendMessage("kill");
+            return 0;
+        } else {
+            ipc.SendMessage(argv[1]);
+            return 0; 
         }
-    }
-    
-    if(argc > 0 && argv[1] != NULL) {
-        track = argv[1];
+    } else if(ipc.isOnlyInstance()) {
+        
+        if(argv[1] == NULL) {
+            
+            perror("Error: track name invalid");
+            return -2;
+        }
+        
+        std::string music_dir;
+        const char* home_dir;
+        std::string track = argv[1];
+        
+        //Get home directory of user
+        home_dir = getenv("HOME");
+        if(home_dir == NULL) {
+            //Get home directory if it is not defined in the environment variable
+            home_dir = getpwuid(getuid())->pw_dir;
+            if(home_dir == NULL) {
+                perror("Error: could not get home directory");
+                return -1;
+            }
+        }
+        
         music_dir = home_dir;
         music_dir += "/Music/";
         music_dir += track;
-    } else {
-        perror("Error: track name invalid");
-        return -2;
-    }
-
-    Sound song;
-
-    IPC ipc("/tmp/fifo");
-
-    if(!ipc.isOnlyInstance() && argv[1] == NULL) {
         
-        ipc.SendMessage("kill");
-        return 0;
-    } else if(!ipc.isOnlyInstance() && argv[1] != NULL) {
-        
-        ipc.SendMessage(argv[1]);
-        return 0;
-    } else if(ipc.isOnlyInstance()) {
-        
+        Sound song;
         std::cout << "Playing file: " << track << std::endl;
         song.createSound(music_dir.c_str());
         song.playSound(false);
         
         std::string msg;
+        bool running = true;
         
         while(song.isPlaying() && running) {
             //GetMessage() is blocking which might need to be changed
             msg = ipc.GetMessage();
             
+            //Events
             if(msg == "kill") {
                 std::cout << "Killed!" << std::endl;
                 running = false;
@@ -77,8 +75,6 @@ int main( int argc, char *argv[]) {
         }
         
         std::cout << track << " stopped, closing." << std::endl;
-        //Currently no need to delete
-        //delete(&ipc);
         return 0;
     }
 }
