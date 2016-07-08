8fb4d03f6522409b81a31ae2f25374570c2caae2
monsterjamp <monsterjamp@yahoo.com>
2016-07-07 20:14:22 -0500

    main.cpp: More spacing and consistency

--- src/main.cpp
+++ src/main.cpp
@@ -16,14 +16,14 @@
 #include <pwd.h>
 #include <sys/types.h>
 
-int main( int argc, char *argv[]){
+int main( int argc, char *argv[]) {
 
-  std::string track;
-  std::string music_dir;
-  const char* home_dir;
-  bool running = true;
+    std::string track;
+    std::string music_dir;
+    const char* home_dir;
+    bool running = true;
 
-  std::string kill = "kill";
+    std::string kill = "kill";
   
     //Get home directory of user
     home_dir = getenv("HOME");
@@ -46,62 +46,61 @@ int main( int argc, char *argv[]){
         return -2;
     }
 
-  Sound song;
+    Sound song;
 
-  IPC ipc("/tmp/fifo");
+    IPC ipc("/tmp/fifo");
 
-  //Checks if this the only instance
-  if( argc == 2 && ipc.isOnlyInstance()){
+    //Checks if this the only instance
+    if(argc == 2 && ipc.isOnlyInstance()) {
 
-    std::cout<< "Playing file -" + track <<std::endl;
-    song.createSound( music_dir.c_str());
-    song.playSound( false);
+        std::cout << "Playing file -" + track << std::endl;
+        song.createSound( music_dir.c_str());
+        song.playSound(false);
 
-    std::cout<< "Program is running"<<std::endl;
+        std::cout << "Program is running" << std::endl;
 
-    
-  //might want to slow this down later too
-  while( song.isPlaying() && running){
+        //might want to slow this down later too
+        while(song.isPlaying() && running){
 
-    //will listen for IPC calls and process them here
-    if( ipc.GetMessage() == "kill" ) {
-      std::cout << "~~~~~~~~~~~~~~~~~~~~~~~\n" << std::endl;
-      running = false;
+            //will listen for IPC calls and process them here
+            if(ipc.GetMessage() == "kill" ) {
+                  std::cout << "~~~~~~~~~~~~~~~~~~~~~~~\n" << std::endl;
+                  running = false;
+            }
+        sleep(10);
     }
-    sleep( 10);
-  }
 
-    std::cout<< track + " stopped, closing." <<std::endl;
+    std::cout << track + " stopped, closing." << std::endl;
     delete(&ipc);
     return 0;
 
-  //Send message then quit
-  }else if( argc == 2 && !ipc.isOnlyInstance()){
+    //Send message then quit
+    } else if(argc == 2 && !ipc.isOnlyInstance()) {
 
-    //sends the message that was in args
-    ///FOR NOW SIMPLY KILLS THE PROGRAM
-    ipc.SendMessage( kill.c_str());
-    delete(&ipc);
-    return 0;
-  }else{
-
-    //if no argument, or one that is not a recognised command
-    //is passed to this instande and an original instance is allready running
-    //then send the kill command to shut the original program down
-    if( !ipc.isOnlyInstance()){
-
+        //sends the message that was in args
+        ///FOR NOW SIMPLY KILLS THE PROGRAM
         ipc.SendMessage( kill.c_str());
-    std::cout<< kill.c_str();
+        delete(&ipc);
+        return 0;
+    } else {
 
-    //else the program was called with bad arguments or without any
-    //displaying notes on working commands
-    }else{
+        //if no argument, or one that is not a recognised command
+        //is passed to this instande and an original instance is allready running
+        //then send the kill command to shut the original program down
+        if(!ipc.isOnlyInstance()){
 
-     //will output a list of commands when is completed
-     std::cout<< "\n-Type \"revengeMusic -COMMAND\" eg:\n";
+            ipc.SendMessage(kill.c_str());
+            std::cout << kill.c_str();
+
+        //else the program was called with bad arguments or without any
+        //displaying notes on working commands
+        } else {
+
+            //will output a list of commands when is completed
+            std::cout << "\n-Type \"revengeMusic -COMMAND\" eg:\n";
+        }
+
+        delete(&ipc);
+        return 0;
     }
-
-    delete(&ipc);
-    return 0;
-  }
 }
