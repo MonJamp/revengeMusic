df74efe5a77def9391b6ea65a231187aebe679d6
monsterjamp <monsterjamp@yahoo.com>
2016-07-14 01:38:32 -0500

    Implement Next/Prev (#12)
    * Store list of sound files
    
    * Implement Next/Prev song

--- include/Sound.h
+++ include/Sound.h
@@ -3,11 +3,14 @@
 
 #include <fmod.hpp>
 
+#include <vector>
+#include <string>
+
 class Sound{
 
     public:
         
-        Sound();
+        Sound(const char* music_dir);
         ~Sound();
         
         bool init();
@@ -17,9 +20,15 @@ class Sound{
         void play();
         void play(const char* track_dir);
         void pause();
+        void play_next();
+        void play_prev();
         
     private:
-    
+        
+        void getFileList(const char* music_dir);
+        
+        std::vector<std::string> filelist;
+        std::vector<std::string> playedFiles;
         FMOD::Channel *channel;
         FMOD::Sound *newSound;
         FMOD::System *m_pSystem;
