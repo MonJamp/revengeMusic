6e03865447d6187f5b117823dddec2a1100aeeca
Dyrand <Dyrand@inbox.com>
2016-07-16 12:57:14 -0400

    Changed for loop for consistency

--- src/main.cpp
+++ src/main.cpp
@@ -59,7 +59,7 @@ int main( int argc, char *argv[]) {
         std::string subdirectory;
 
 		//Check command line arguments
-        for(int i(1); i < argc; ++i)
+        for(int i = 1; i < argc; ++i)
         {
           if(strcmp(argv[i],"-subdir") == 0)
           {
