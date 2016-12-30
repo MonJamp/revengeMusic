ceeae123cdb8dd0c34a7646fc46cbba0ac439677
FiljoA <monsterjamp@yahoo.com>
2016-12-30 11:58:59 -0600

    Log boost exceptions

--- include/Logger.h
+++ include/Logger.h
@@ -1,14 +1,18 @@
 #pragma once
 
+#include <boost/interprocess/exceptions.hpp>
+
 #include <string>
 #include <sstream>
 
 namespace Logger {
     void SetError(std::string msg);
     void PrintError(std::string msg = "Unknown"); //Can also set errors
+    void PrintError(boost::interprocess::interprocess_exception &ex,
+                    std::string msg = "Unknown");
     void Log(); //Toggles redirecting error messages to log file
     
     extern std::stringstream error_msg;
     extern bool error_set;
-	extern bool logging;
+    extern bool logging;
 }
